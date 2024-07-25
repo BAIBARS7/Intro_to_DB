e383c2a14b96a420939cc71b82685568051baca8
BAIBARS7 <159480461+BAIBARS7@users.noreply.github.com>
2024-07-26 00:04:09 +0300

    Update 4 task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,9 @@
--- Insert multiple rows into the customer table
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- This script inserts multiple rows into the customer table
 
+USE `$1`;  -- Use the database name passed as argument
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
+  (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+  (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+  (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
